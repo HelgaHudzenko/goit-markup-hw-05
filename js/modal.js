53816e8dad71c4567572b74caaa7215f607f4c5e
HelgaHudzenko <olichka.og85@gmail.com>
2022-11-20 17:20:13 +0200

    edit

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,14 @@
-let modal = document.getElementById('myModal');
-		let btn = document.getElementById('myBtn');
-		let span = document.getElementsByClassName("close")[0];
-
-		btn.onclick = function () {
-			modal.style.display = "block";
-		}
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
+  (() => {
+	const refs = {
+	  openModalBtn: document.querySelector("[data-modal-open]"),
+	  closeModalBtn: document.querySelector("[data-modal-close]"),
+	  modal: document.querySelector("[data-modal]"),
+	};
+  
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+	function toggleModal() {
+	  refs.modal.classList.toggle("is-hidden");
+	}
+  })();
